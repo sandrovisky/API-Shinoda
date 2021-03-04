2bad84867d1a09866e0f7b92e1acd797cc8ce770
Sandro Garcia <sndr95@hotmail.com>
2021-03-04 16:29:24 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -6,15 +6,15 @@ require('./database/index')
 
 const app = express()
 
-app.use(cors({ origin: 'https://shinoda-app.herokuapp.com/' , credentials :  true,  methods: 'GET,PUT,POST,OPTIONS', allowedHeaders: 'Content-Type,Authorization' }))
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://shinoda-app.herokuapp.com/")
-    res.header("Access-Control-Allow-Headers", "https://shinoda-app.herokuapp.com/")
-    res.header("Access-Control-Allow-Methods", "https://shinoda-app.herokuapp.com/")
+app.use(cors())
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*")
+//     res.header("Access-Control-Allow-Headers", "*")
+//     res.header("Access-Control-Allow-Methods", "*")
     
-    app.use(cors)
-    next()
-})
+//     app.use(cors)
+//     next()
+// })
 
 app.use(express.json())
 
