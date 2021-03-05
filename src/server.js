93e7d75087a3a9c6054f6333b69c529cff4d6e6a
Sandro Garcia <sndr95@hotmail.com>
2021-03-04 21:14:19 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,7 +9,9 @@ const app = express()
 app.use(express.json())
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://shinoda-app.herokuapp.com/")
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "*")
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
     app.use(cors())
     next()
 })
