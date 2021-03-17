0ae2d6873fba2856df6750cbfdffc224398381dc
Sandro Garcia <sndr95@hotmail.com>
2021-03-17 19:49:33 -0300

    Update auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,26 +1,25 @@
 const jwt = require('jsonwebtoken')
 
 module.exports = ( req, res, next ) => {
-    // const authHeader = req.headers["authorization"]
+    const authHeader = req.headers["authorization"]
 
-    // if ( !authHeader )
-    //     return res.status(401).send({ error: "Token não encontrado!"})
+    if ( !authHeader )
+        return res.status(401).send({ error: "Token não encontrado!"})
     
-    // const parts = authHeader.split(' ')
+    const parts = authHeader.split(' ')
 
-    // if ( !parts.length === 2 )
-    //     return res.status(401).send({ error: "Token error"})
+    if ( !parts.length === 2 )
+        return res.status(401).send({ error: "Token error"})
 
-    // const [ scheme, token ] = parts
+    const [ scheme, token ] = parts
 
-    // if ( !/^Bearer$/i.test(scheme ))
-    //     return res.status(401).send({ error: "Token erro de formatação"})
+    if ( !/^Bearer$/i.test(scheme ))
+        return res.status(401).send({ error: "Token erro de formatação"})
         
-    // jwt.verify( token, process.env.HASH_JWT, (err, decoded) => {
-    //     if ( err ) return res.status(401).send({ error: "Token invalido"})
+    jwt.verify( token, process.env.HASH_JWT, (err, decoded) => {
+        if ( err ) return res.status(401).send({ error: "Token invalido"})
 
-    //     req.idUsuario = decoded.id
-    //     return next()
-    // })
-    next()
+        req.idUsuario = decoded.id
+        return next()
+    })
 }
